aa96f6164be8ec1ab8980d7b8ef37ff2fcee04ef
aoalharbi1 <51276512+aoalharbi1@users.noreply.github.com>
2019-10-21 11:52:54 +0300

    Recruitment App

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+const bodyParser = require("body-parser");
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: 'keyboardkitteh',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+}));
+
+require('./server/config/mongoose.js');
+require('./server/config/routes.js')(app)
+
+app.listen(8000, () => console.log("listening on port 8000"));
---
+++ server/config/routes.js
@@ -0,0 +1,24 @@
+const jobs = require('../controllers/jobs.js');
+
+module.exports = function (app) {
+    app.get('/jobs', (req, res) => {
+        jobs.index(req, res);
+    });
+
+    app.get('/job', (req, res) => {
+        jobs.getById(req, res);
+    });
+
+    app.post('/job', (req, res) => {
+        jobs.create(req, res);
+    });
+
+    app.put('/job', (req, res) => {
+        jobs.update(req, res);
+    });
+
+    app.delete('/job', (req, res) => {
+        jobs.remove(req, res);
+    });
+
+}
