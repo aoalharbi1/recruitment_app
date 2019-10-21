7bd38fdfabc13961ab350a0cee7476df987b1ea0
Mohammed Almalki <m7mdmallki@gmail.com>
2019-10-21 14:11:49 +0300

    JopSeekerSchama

---
+++ server/models/jobSeeker.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const JopSeekerSchema = new mongoose.Schema({
+    fname: { type: String, required: true },
+    lname: {type: String, required: true},
+    email: {type: String, required: true },
+    password: {type: String, required: true},
+    jops: [Jobs],
+    info: {
+        dateOfbirth : {type: Date, required: true},
+        gender : {type: String, required: true},
+        phone: {tybe: String, required: true},
+        city: {tybe: String, required: true},
+        gpa: {tybe: String, required: true},
+        university: {tybe: String, required: true},
+        major: {tybe: String, required: true},
+        education:{tybe: String, required: true},
+        link: {tybe: String, required: true},
+    },
+    status: {},
+    created_at: {type: Date, default: Date.now()},
+    updated_at: {type: Date, default: Date.now()}
+});
+mongoose.model('Task', TaskSchema);
