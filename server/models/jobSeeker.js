75dd53fb0b3094fc4b26f27837c99aea75826ab9
Mohammed Almalki <m7mdmallki@gmail.com>
2019-10-21 14:12:42 +0300

    JopSekerSchema

--- server/models/jobSeeker.js
+++ server/models/jobSeeker.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const JopSeekerSchema = new mongoose.Schema({
-    fname: { type: String, required: true },
-    lname: {type: String, required: true},
+    first_name: { type: String, required: true },
+    last_name: {type: String, required: true},
     email: {type: String, required: true },
     password: {type: String, required: true},
     jops: [Jobs],
