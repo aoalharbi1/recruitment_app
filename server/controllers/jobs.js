9592c78c4296040ae19a32d4083c3fa365caeab3
aoalharbi1 <51276512+aoalharbi1@users.noreply.github.com>
2019-10-27 09:50:02 +0300

    Update jobs.js

--- server/controllers/jobs.js
+++ server/controllers/jobs.js
@@ -2,25 +2,12 @@ const mongoose = require('mongoose');
 const Job = mongoose.model('Job');
 const Recruiter = mongoose.model('Recruiter');
 const JobSeeker = mongoose.model('JobSeeker');
-const nodemailer = require('nodemailer');
 
 module.exports = {
     getAll: (req, res) => {
         Job.find({}, { 'applied_users': 0 })
             .then(jobs => res.json(jobs))
             .catch(err => res.json(err));
-
-            const receiver = {
-                first_name: "Abdullah",
-                last_name: "Alharbi",
-                email: "abaaady.aa@gmail.com"
-            }
-
-            const job = {
-                title: "Software Enginer",
-                company: "SITE"
-            }
-            mail(receiver, job)
     },
 
     getById: (req, res) => {
@@ -64,61 +51,3 @@ module.exports = {
             .catch(err => res.json(err));
     }
 } 
-
-
-function mail(receiver, job) {
-
-    
-    const transporter = nodemailer.createTransport({
-        // do not forget to give permission for third party in gmail
-        /*
-         1- login in gmail via your browser
-         2- after you are logged in paste this link in your browser https://myaccount.google.com/lesssecureapps
-         3- Allow less secure apps: ON
-        */
-
-        service: 'gmail',
-        auth: {
-            user: 'testingbrightfuture@gmail.com‏', //Email account
-            pass: '12345678Ww@' // email password
-        }
-    });
-
-    const mailOptions = {
-        //(the sender)
-        from: 'testingbrightfuture@gmail.com‏',
-        // (the receiver)
-        to: receiver.email,   // */ anyMail
-
-        // titile
-        subject: `Applied to ${job.title}`,
-
-        //body message // for multi lines use this symbol ` `
-        text: `
-        Dear ${receiver.first_name} ${receiver.last_name},
-
-        Thank you for applying for ${job.title} posted by ${job.company}. We wish you the best of luck.
-
-        Best regards,
-        Bright Future` ,
-
-        // attachments   https://nodemailer.com/message/attachments/
-        attachments: [
-            {   // file as an attachment
-                filename: 'test.pdf',
-                path: __dirname + '/files' // stream this file
-            }
-        ]
-
-    };
-
-    transporter.sendMail(mailOptions, (errorHappned, info) => {
-        if (errorHappned) {
-            console.log(errorHappned);
-        }
-        else {
-            console.log('Email sent: ' + info.response);
-        }
-
-    });
-}
