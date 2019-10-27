6c2275903cd45aa75133d6974d8ead04227b7c16
Nawaf <30578987+The-7-Hunter@users.noreply.github.com>
2019-10-27 13:13:14 +0300

    show users applied for the jobs

--- public/src/app/components/recruiter/job-posted/job-posted.component.ts
+++ public/src/app/components/recruiter/job-posted/job-posted.component.ts
@@ -8,9 +8,12 @@ import { HttpService } from '../../../http.service';
 })
 export class JobPostedComponent implements OnInit {
   jobs: any;
+  modal_info: any;
+  applied: any;
   constructor(private http: HttpService) { }
 
   ngOnInit() {
+    this.modal_info = {};
     this.recJobs();
   }
 
@@ -20,5 +23,16 @@ export class JobPostedComponent implements OnInit {
       console.log(data);
       this.jobs = data;
     })
+  }
+
+  getModalInfo (job){
+    console.log("Users", job.applied_users);
+    this.modal_info = job;
+    this.applied = job.applied_users;
+    
+  }
+
+  getUsers(job){
+
   }
 }
